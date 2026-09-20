f0c8f18c8a5cb137b69adbd019bc75a7cceeae66
ariensherwen <ariensherwen@gmail.com>
2026-09-20 18:45:51 +0700

    test(plugins): use isolated package store in e2e

--- tests/plugin_e2e.rs
+++ tests/plugin_e2e.rs
@@ -35,7 +35,14 @@ async fn manager() -> (PluginManager, Pool) {
     db::migrate(&pool).await.unwrap();
     let crypto = Arc::new(Crypto::new(&[7u8; 32]));
     let http = reqwest::Client::new();
-    let manager = PluginManager::new(pool.clone(), crypto, http, HostPolicy::default()).unwrap();
+    let manager = PluginManager::new(
+        pool.clone(),
+        crypto,
+        http,
+        HostPolicy::default(),
+        dir.join("plugin-packages"),
+    )
+    .unwrap();
     (manager, pool)
 }
 
