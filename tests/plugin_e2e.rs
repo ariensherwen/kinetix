e7cc6654a8d9c6a930ac843704733d167cb7b19c
ariensherwen <ariensherwen@gmail.com>
2026-09-20 21:01:57 +0700

    test(plugins): update manager constructor

--- tests/plugin_e2e.rs
+++ tests/plugin_e2e.rs
@@ -34,11 +34,9 @@ async fn manager() -> (PluginManager, Pool) {
     let pool = db::connect(&url).await.unwrap();
     db::migrate(&pool).await.unwrap();
     let crypto = Arc::new(Crypto::new(&[7u8; 32]));
-    let http = reqwest::Client::new();
     let manager = PluginManager::new(
         pool.clone(),
         crypto,
-        http,
         HostPolicy::default(),
         dir.join("plugin-packages"),
     )
