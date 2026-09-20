cdd38b2dd3b2a51c14c7d171dbb9866876712881
ariensherwen <ariensherwen@gmail.com>
2026-09-20 17:38:24 +0700

    feat(plugins): export integration descriptor type

--- src/plugins/mod.rs
+++ src/plugins/mod.rs
@@ -19,6 +19,6 @@ pub use manager::PluginManager;
 
 pub use manifest::{HostPolicy, ValidatedManifest};
 pub use types::{
-    Capability, CircuitState, Limits, Manifest, Permissions, PluginRef, PluginStatus, Provided,
+    Capability, CircuitState, Integration, Limits, Manifest, Permissions, PluginRef, PluginStatus, Provided,
     MANIFEST_VERSION, PLUGIN_API_MAJOR,
 };
