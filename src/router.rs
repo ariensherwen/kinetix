894f4bf78fcd64bcec1c7eed757e5ef3db066d8d
ariensherwen <ariensherwen@gmail.com>
2026-09-20 19:21:45 +0700

    feat(admin): route plugin settings API

--- src/router.rs
+++ src/router.rs
@@ -136,6 +136,10 @@ pub fn build(state: AppState) -> Router {
         .route("/plugins/{id}/enable", post(admin::enable_plugin))
         .route("/plugins/{id}/disable", post(admin::disable_plugin))
         .route("/plugins/{id}/validate", post(admin::validate_plugin))
+        .route(
+            "/plugins/{id}/settings",
+            get(admin::plugin_settings).put(admin::update_plugin_settings),
+        )
         .route("/plugins/{id}/permissions", get(admin::plugin_permissions))
         .route(
             "/plugins/{id}/permissions/approve",
